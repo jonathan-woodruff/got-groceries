e68614d527cc236e48d09b132ef7bdf9789ee61a
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-03-17 16:03:56 -0500

    added app.js in the routes folder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
 
+
 const router = createBrowserRouter(createRoutesFromElements(
   <>
     <Route path='/' element={ <SignIn /> } />
